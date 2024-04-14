bf0cc6be5773146fee4f7129906f86e61e8d022e
rouabah <rouabah.zineb@hotmail.fr>
2024-04-14 23:01:16 +0200

    modif competences

--- src/components/Competences/Competences.jsx
+++ src/components/Competences/Competences.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState, useRef } from 'react';
 import styles from './Competences.module.css';
 import skills from '../../data/skills.json';
-import historyFr from '../../data/history.fr.json'; // Importez le fichier JSON en français
 import { getImageUrl } from '../../utils';
 
 export const Competences = () => {
-  const [history, setHistory] = useState([]);
   const [isVisible, setIsVisible] = useState(false);
   const competencesRef = useRef(null);
 
   useEffect(() => {
-    // Utilisez les données importées directement du fichier JSON en français
-    setHistory(historyFr.history || []);
-
     const handleScroll = () => {
       if (competencesRef.current) {
         const topOffset = competencesRef.current.getBoundingClientRect().top;
@@ -22,13 +17,9 @@ export const Competences = () => {
       }
     };
 
-    // Écouter le scroll de la fenêtre
     window.addEventListener('scroll', handleScroll);
-
-    // Vérifier si la section est visible au montage initial
     handleScroll();
 
-    // Nettoyer l'écouteur d'événements lors du démontage
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -44,7 +35,6 @@ export const Competences = () => {
     <section className={styles.container} id='competences' ref={competencesRef}>
       <h2 className={styles.title}>Compétences</h2>
       <div className={styles.content}>
-        {/* Render Skills uniquement si la section est visible */}
         <div className={styles.skills}>
           {skills.map((skill, id) => (
             <div key={id} className={styles.skill}>
