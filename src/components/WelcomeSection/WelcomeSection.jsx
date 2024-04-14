4722e69091ef4ee7a5f0c85fcbf7b2dd606ffd25
rouabah <rouabah.zineb@hotmail.fr>
2024-04-14 23:59:02 +0200

    deplacement photo du fond au fichier react

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './WelcomeSection.module.css';
 import { getImageUrl } from '../../utils';
+import backgroundImage from '/assets/photo_portfolio/background_image.png'; // Importez votre image de fond
 
 const WelcomeSection = () => {
   const handleContactClick = () => {
@@ -12,7 +13,7 @@ const WelcomeSection = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
       <div className={styles.textSection}>
         <p>Bonjour,</p>
         <p>Je suis Zineb Rouabah</p>
@@ -20,6 +21,7 @@ const WelcomeSection = () => {
         <button className={styles.btnHere} onClick={handleContactClick}>me contacter</button>
         <button className={styles.btnDownload} onClick={handleDownloadClick}>mon CV</button>
       </div>
+      {/* L'image du modèle peut rester dans votre fichier CSS */}
       <img src={getImageUrl('photo_portfolio/photo_portfolio.jpg')} alt="Photo de profil" className={styles.model} />
     </div>
   );
